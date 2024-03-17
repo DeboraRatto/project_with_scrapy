8f13414c7f79cb1556657b4b42d425ad21392eaa
Debora Ratto <drattodourado@gmail.com>
2024-03-17 09:37:28 -0300

    load atualizado

--- Pipeline/load.py
+++ Pipeline/load.py
@@ -4,26 +4,35 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 
 
-# Obtendo o diretório atual do script
-dir_atual = os.path.dirname(__file__)
-# Construindo o caminho completo para o arquivo JSON
-caminho_json_news = os.path.join(dir_atual, 'extract', 'g1news.json')
+def authenticate_bigquery(credential_path):
+    return service_account.Credentials.from_service_account_file(credential_path)
 
-# Autenticando com as credenciais do Google Cloud
-credentials = service_account.Credentials.from_service_account_file('Credentials\credentials_bigquery.json')
+def load_dataframe(extract_json_path):
+    return pd.read_json(extract_json_path)
+
+def load_data_bigquery(df, project_id, data_set_id, table_name, credentials):
+    load_bq= df.to_gbq(destination_table=f'{project_id}.{data_set_id}.{table_name}',
+                    project_id=project_id,
+                    credentials=credentials,
+                    if_exists='replace')  # Substituir a tabela se ela já existir
+    return load_bq
+
+# Obtem o diretório atual do script, o caminho do json extract e caminho das credenciais
+dir_atual = os.path.dirname(__file__)
+json_path_news = os.path.join(dir_atual, 'extract', 'g1news.json')
+if not os.path.exists(json_path_news): #verifica a existencia
+    raise FileNotFoundError(f'O arquivo JSON {json_path_news} não foi encontrado.')
+
+dir_credentials = os.path.join(dir_atual, '..', 'Credentials')
+credential_path = os.path.join(dir_credentials, 'credentials_bigquery.json')
 
 # Definindo as informações da tabela do BigQuery
-projeto_id = 'smart-rope-417322'
-conjunto_de_dados_id = 'g1_news'
-nome_tabela = 'News'
+project_id = 'smart-rope-417322'
+data_set_id = 'g1_news'
+table_name = 'News'
 
-# Carregando o DataFrame do pandas
-df = pd.read_json(caminho_json_news)
+Credentials= authenticate_bigquery(credential_path)
 
-# Carregando o DataFrame na tabela do BigQuery
-df.to_gbq(destination_table=f'{projeto_id}.{conjunto_de_dados_id}.{nome_tabela}',
-          project_id=projeto_id,
-          credentials=credentials,
-          if_exists='replace')  # Substituir a tabela se ela já existir
+df= load_dataframe(json_path_news)
 
-
+load_data_bigquery(df, project_id, data_set_id, table_name,Credentials)
